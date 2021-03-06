712a13232374735f6a68111fd7bce97e69bd4b52
Pattounes <77328374+ldrt@users.noreply.github.com>
2021-03-06 15:47:57 +0100

    Search by city and paginate

---
+++ src/URLHelper.php
@@ -0,0 +1,9 @@
+<?php
+namespace App;
+
+class URLHelper {
+    public static function withParam(string $param, $value) : string
+    {
+        return http_build_query(array_merge($_GET, [$param => $value]));
+    }
+}
